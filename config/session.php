606db54ab861f5ca687841db22d04c4f42bfe809
biohazardhome <stalker-nikko@yandex.ru>
2016-06-23 12:17:21 -0400

    Settings session name

--- config/session.php
+++ config/session.php
@@ -109,7 +109,8 @@ return [
     |
     */
 
-    'cookie' => 'laravel_session',
+    // 'cookie' => 'laravel_session',
+    'cookie' => 'avto_session',
 
     /*
     |--------------------------------------------------------------------------
