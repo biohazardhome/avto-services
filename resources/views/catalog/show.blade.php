e1308ea6bc13f607fa51489594384e9eb6d59010
Bio <biohazardhome@gmail.com>
2016-07-19 17:56:35 +0300

    Changed visual entity catalog, comments, map

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -4,31 +4,40 @@
 @section('description', $catalog->description)
 
 @section('content')
-	<section class="col-md-9 col-lg-9">
-		<article class="catalog-item">
-			@include('catalog.partials.item-header', $catalog->getAttributesOnly(['name', 'slug']))
-			@include('catalog.partials.item-address', $catalog->getAttributesOnly(['name', 'address']))
+	<section class="catalog-show col-md-9 col-lg-9">
+		<article class="catalog-item bg-gray">
+			<div class="catalog-item-well">
+				@include('catalog.partials.item-header', $catalog->getAttributesOnly(['name', 'slug']))
+				@include('catalog.partials.item-address', $catalog->getAttributesOnly(['name', 'address']))
 
-			<div class="catalog-description">{!! $catalog->description !!}</div>
+				<div class="catalog-description">{!! $catalog->description !!}</div>
 
-			@include('catalog.partials.item-info', $catalog->getAttributesOnly(['phones', 'site', 'email', 'name']))
+				@include('catalog.partials.item-info', $catalog->getAttributesOnly(['phones', 'site', 'email', 'name']))
 
-			<div class="catalog-content">{!! $catalog->content !!}</div>
-			<div class="catalog-map">
+				<div class="catalog-content">{!! $catalog->content !!}</div>
+			</div>
+
+			<div class="catalog-map catalog-item-well">
+				<a name="map"></a>
 				<h2>Автосервис "{{ $catalog->name }}" на карте</h2>
 				@include('partials.map', ['catalog' => $catalogMap])
 				<div id="map" style="width:auto; height: 450px;"></div>
 			</div>
 
-			<div class="catalog-comment">
-				<h2>
-					Автосервис "{{ $catalog->name }}" отзывы
-				</h2>
-				@include('comment.create', ['catalogId' => $catalog->id])
+			<div class="catalog-comment catalog-item-well">
+				<a name="comments"></a>
 				
-				@foreach($catalog->comments as $comment)
-					@include('comment.show', $comment)
-				@endforeach
+				<div class="comment-actions">
+					<div class="comment-actions pull-right">
+						<a href="#" class="comment-form-show btn btn-default" title="Показать форму отзывов">Написать</a>
+					</div>
+					<h2>
+						Отзывы о Автосервисе {{ $catalog->name }} в Одинцово
+					</h2>
+				</div>
+				@include('comment.create', ['catalogId' => $catalog->id])
+				@include('comment.index', ['comments' => $catalog->comments])
+				
 			</div>
 		</article>
 		
@@ -36,36 +45,37 @@
 @stop
 
 @section('sidebar')
-	
-	<div class="catalog-similar">
-		@inject('similar', 'App\Catalog')
-		
-		<h3>Автосервисы на {{ $catalog->addressStreet }}</h3>
-		<ul class="catalog-list-similar">
-			@foreach($similar->LikeByAddress($catalog->addressStreet, 3) as $item)
-				<li>
-					<article class="catalog-item">
-						@include('catalog.partials.item-header-link', $item->getAttributesOnly(['name', 'slug']))
-						@include('catalog.partials.item-address', $item->getAttributesOnly(['name', 'address']))
-					</article>
-				</li>
-			@endforeach
-		</ul>
-	</div>
+	<aside class="col-md-3 col-lg-3">
+		<div class="catalog-similar">
+			@inject('similar', 'App\Catalog')
+			
+			<h3>Автосервисы на {{ $catalog->addressStreet }}</h3>
+			<ul class="catalog-list-similar">
+				@foreach($similar->LikeByAddress($catalog->addressStreet, 3) as $item)
+					<li>
+						<article class="catalog-item">
+							@include('catalog.partials.item-header-link', $item->getAttributesOnly(['name', 'slug']))
+							@include('catalog.partials.item-address-anchor', $item->getAttributesOnly(['slug', 'name', 'address']))
+						</article>
+					</li>
+				@endforeach
+			</ul>
+		</div>
 
-	<div class="catalog-similar">
-		@inject('similar', 'App\Catalog')
-		
-		<h3>Автосервисы в {{ $catalog->addressCity }}</h3>
-		<ul class="catalog-list-similar">
-			@foreach($similar->LikeByAddress($catalog->addressCity, 3) as $item)
-				<li>
-					<article class="catalog-item">
-						@include('catalog.partials.item-header-link', $item->getAttributesOnly(['name', 'slug']))
-						@include('catalog.partials.item-address', $item->getAttributesOnly(['name', 'address']))
-					</article>
-				</li>
-			@endforeach
-		</ul>
-	</div>
+		<div class="catalog-similar">
+			@inject('similar', 'App\Catalog')
+			
+			<h3>Автосервисы в {{ $catalog->addressCity }}</h3>
+			<ul class="catalog-list-similar">
+				@foreach($similar->LikeByAddress($catalog->addressCity, 3) as $item)
+					<li>
+						<article class="catalog-item">
+							@include('catalog.partials.item-header-link', $item->getAttributesOnly(['name', 'slug']))
+							@include('catalog.partials.item-address-anchor', $item->getAttributesOnly(['slug', 'name', 'address']))
+						</article>
+					</li>
+				@endforeach
+			</ul>
+		</div>
+	</aside>
 @stop
