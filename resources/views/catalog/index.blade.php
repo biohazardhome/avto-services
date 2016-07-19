7dee5260ee483512afe610a9a1134baafc6ad745
Bio <biohazardhome@gmail.com>
2016-07-19 17:07:35 +0300

    Changed count comments for entity catalog and link on address (anchor)

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -4,9 +4,8 @@
 @section('description', 'Автосервисы в Одинцово - каталог адресов и телефонов автосервисов в Одинцово с полной справочной информацией и отзывами') <!-- полная справочная информация, схема проезда-->
 
 @section('content')
-	
 
-	<section class="catalog-list col-md-9 col-lg-9">
+	<section class="catalog-list col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2">
 		<h1 class="text-center">Автосервисы в Одинцово</h1>
 
 		<p>
@@ -18,12 +17,13 @@
 		@foreach($catalog as $item)
 			<article class="catalog-item">
 				@include('catalog.partials.item-header-link', $item->getAttributesOnly(['name', 'slug']))
-				@include('catalog.partials.item-address', $item->getAttributesOnly(['name', 'address']))
+				@include('catalog.partials.item-address-anchor', $item->getAttributesOnly(['slug', 'name', 'address']))
 				<div class="catalog-item-content" title="Подробнее об автосервисе {{ $item->name }}">
 					{!! $item->description !!}
 				</div>
 				@include('catalog.partials.item-info', $item->getAttributesOnly(['phones', 'site', 'email', 'name']))
-				<a href="#" title="Отзывы о автосервисе {{ $item->name }}">Отзывы</a>
+
+				<a href="{{ route('catalog.show', [$item->slug]) }}#comments" title="Отзывы о автосервисе {{ $item->name }}">Отзывы ({{ $item->comments_count }})</a>
 			</article>
 		@endforeach
 	
--- resources/views/catalog/partials/item-address.blade.php
+++ resources/views/catalog/partials/item-address.blade.php
@@ -1,4 +1,4 @@
 <address>
 	<span class="glyphicon glyphicon-map-marker"></span>
-	<a href="{{ route('map.show', [$name, $address]) }}" title="Узнать расположение автосервиса {{ $name }} на карте" target="_blank">{{ $address }}</a>
+	<a href="{{ route('map.show', [$name, $address]) }}" title="Узнать расположение автосервиса {{ $name }} на карте">{{ $address }}</a>
 </address>
