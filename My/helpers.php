5556215c186e09c303bc797cbdb09199a57b802a
Bio <biohazardhome@gmail.com>
2016-07-23 00:49:11 +0300

    Added function mb_ucfirst()

--- My/helpers.php
+++ My/helpers.php
@@ -10,3 +10,8 @@ function str_limit_with($s, $limit = null, $end = '.', $withEnd = true) {
 	}
 	return $s;
 }
+
+function mb_ucfirst($value) {
+	$value = mb_strtolower(trim($value));
+	return mb_strtoupper(mb_substr($value, 0, 1)) . mb_substr($value, 1);
+}
