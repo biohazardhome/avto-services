1d46679d6d91f479db2872037860dd63641ec251
Bio <biohazardhome@gmail.com>
2016-08-02 21:38:10 +0300

    Added Breadcrumbs

--- resources/views/catalog/city.blade.php
+++ resources/views/catalog/city.blade.php
@@ -3,9 +3,24 @@
 @section('title', $city->title)
 @section('description', $city->description) <!-- полная справочная информация, схема проезда-->
 
+
+@section('head')
+	<link rel="canonical" href="{{ route('catalog.index') }}/" />
+@stop
+
 @section('content')
 
 	<section class="catalog-list col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2">
+		
+		<?php
+			Breadcrumbs::setCssClasses('breadcrumb');
+			Breadcrumbs::setDivider('');
+			Breadcrumbs::add('Автосервисы', '/');
+			Breadcrumbs::add($city->name, '/'. $city->slug);
+		?>
+
+		{!! Breadcrumbs::render() !!}
+
 		<h1 class="text-center">Автосервисы в {{ $city->name }}</h1>
 
 		<p>{{ $city->text }}</p>
