0fe2d341ec97b9737e3db139cff5518478b4d393
Bio <biohazardhome@gmail.com>
2016-08-04 15:00:26 +0300

    Refactoring

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,6 +6,8 @@
 
 	@include('admin.searchable', compact('table'))
 	@include('admin.sortable', compact('table', 'columns'))
+
+	<a class="btn btn-success" href="{{ route('admin.'. $table .'.create') }}" title="">Create</a>
 	
 	{!! $content !!}
 @stop
--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -4,6 +4,15 @@
 	<?php $table = str_singular($model->getTable()); ?>
 	<a class="btn btn-success" href="{{ route('admin.'. $table .'.create') }}" title="">Create</a>
 	<a class="btn btn-success" href="{{ route('admin.'. $table .'.edit', [$model->id]) }}" title="">Edit</a>
+	
+	<?php $routeName = $table .'.show'; ?>
+	@if(Route::has($routeName))
+		<?php
+			$params = [];
+			$params[] = $table === 'catalog' ? $model->slug : $model->id;
+		?>
+		<a class="btn btn-success" href="{{ route($routeName, $params) }}" title="">Show</a>
+	@endif
 
 	<table class="table table-striped table-bordered table-hover table-condensed table-responsive">
 		<thead>
