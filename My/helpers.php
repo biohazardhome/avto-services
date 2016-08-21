c9b47e347e0788bad6a876ca7c6d3ab9eb136c00
Bio <biohazardhome@gmail.com>
2016-08-21 21:49:36 +0300

    declension word after number

--- My/helpers.php
+++ My/helpers.php
@@ -4,6 +4,12 @@ function command($command, array $args = array()) {
 	return call_user_func_array([$command, 'handle'], $args);
 }
 
+// Склонение слова по числу
+function decline_number($number, $after) {
+  $cases = array (2, 0, 1, 1, 1, 2);
+  return $after[ ($number%100>4 && $number%100<20)? 2: $cases[min($number%10, 5)] ];
+}
+
 function str_limit_with($s, $limit = null, $end = '.', $withEnd = true) {
 	$s = substr($s, 0, $limit ? $limit : strlen($s));
 	// var_dump($s);
